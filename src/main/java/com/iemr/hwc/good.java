ed014aa07f2dc02b569ee5c09efa3fbcabc182da
HARSH <harshsennn@gmail.com>
2025-01-10 01:16:02 +0530

    pass check

---
+++ src/main/java/com/iemr/hwc/good.java
@@ -0,0 +1,27 @@
+package com.example;
+
+/**
+ * Example of properly formatted code.
+ */
+public class GoodCode {
+    private final String name;
+    
+    public GoodCode(String name) {
+        this.name = name;
+    }
+    
+    public void processName() {
+        if (name.equals("test")) {
+            System.out.println("Test name found");
+        }
+        
+        boolean isValid = validateName();
+        if (isValid) {
+            System.out.println("Name is valid");
+        }
+    }
+    
+    private boolean validateName() {
+        return name != null && !name.isEmpty();
+    }
+}
